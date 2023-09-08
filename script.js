f5fd05c6ca935797bbf445a6d96c22d152b2f73e
Jdizzydizzle <127226946+Jdizzydizzle@users.noreply.github.com>
2023-09-08 11:10:33 +0100

    put === for matches back to 6

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ function checkForMatch() {
 
     // The settimeout function is used here so the final card can show before the you won message appears
     setTimeout(() => {
-        if (match === 1) {
+        if (match === 6) {
             document.querySelector('div.congratsCounter').textContent = "Took you " + counter.innerHTML +' moves'; 
             // document.querySelector('div.congratsStars').textContent = `You got ${stars1Counter.innerHTML}`; 
             document.querySelector('div.congratsTimer').textContent = " " + timer.textContent + ' seconds';
